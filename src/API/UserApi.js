1bea06dfa7cb05a0db78f451b7476b0becc5b500
KiranSChandannanavar1999 <kkirans57337@gmail.com>
2023-11-20 16:56:39 +0530

    file added

---
+++ src/API/UserApi.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+
+const axiosIns = axios.create({
+baseURL:  'https://node-crud-api-gkh4.onrender.com'
+})
+
+const UserApi = {
+    readAll : () => {
+        return axiosIns.request({
+            url: `/api/user`,
+            method: "GET"
+        })
+    },
+    readSingle : (id) => {
+        return axiosIns.request({
+            url: `/api/user/single/${id}`,
+            method: "GET"
+        })
+    },
+    create : (user) => {
+        return axiosIns.request({
+            url: `/api/user/add`,
+            method: "POST",
+            data: user
+        })
+    },
+    update : (user,id) => {
+        return axiosIns.request({
+            url: `/api/user/update/${id}`,
+            method: "PATCH",
+            data: user
+        })
+    },
+    delete : (id) => {
+        return axiosIns.request({
+            url: `/api/user/delete/${id}`,
+            method: "DELETE"
+        })
+    },
+}
+
+export default UserApi
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Menu from './component/Menu'
+import Home from './component/Home'
+import Create from './component/Create'
+import Update from './component/Update'
+import Pnf from './component/Pnf'
+import { ToastContainer } from 'react-toastify'
+
+
+function App(props) {
+  return (
+    <BrowserRouter>
+    <Menu/>
+    <ToastContainer autoClose={4000} position={'top-right'} />
+    <Routes>
+      <Route path={'/'} element={<Home/>} />
+      <Route path={'/create'} element={<Create/>} />
+      <Route path={'/update/:id'} element={<Update/>} />
+      <Route path={'/*'} element={<Pnf/>} />
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
